0aa3d61537aba29800ec9e03b26723f72c12be94
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2014-09-17 17:20:07 -0600

    testing including an html file in a php file for heroku deployment

---
+++ app/pokemon.js
@@ -0,0 +1,32 @@
+var pokemonApp = angular.module('pokedex', []);
+
+/*pokemonApp.factory('PokemonFactory', function($http) {
+    return $http.get('http://poketest.googlecode.com/svn/trunk/pokemon.json');
+});
+
+pokemonApp.controller('PokemonCtrl', function ($scope, PokemonFactory){
+    //$scope.pokemon = [{id: 1, name: "Bulbasaur"}];
+
+        PokemonFactory.then(function(data){
+            $scope.pokemon = data.mn;
+        },
+        function(error){
+            alert("Pokemon data retrieval was not super effective. :(");
+         });
+}); */
+
+
+
+
+pokemonApp.controller('PokemonController', function($scope, $http){
+    $scope.pokemon = [];
+
+    $http.get('pkmn.json')
+        .success(function(results){
+            $scope.pokemon = results;
+//            alert("this worked");
+        });
+
+    $scope.pokemon = [{num: 1}, {num: 2}, {num: 3}];
+ //   alert("we got here");
+});
