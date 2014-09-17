7d34d78ef20ed1bb52aa68570be3dd0b84b4b240
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2014-09-17 17:39:22 -0600

    Added .swp files to .gitignore. Removed debug statements

--- app/pokemon.js
+++ app/pokemon.js
@@ -1,32 +1,14 @@
 var pokemonApp = angular.module('pokedex', []);
 
-/*pokemonApp.factory('PokemonFactory', function($http) {
-    return $http.get('http://poketest.googlecode.com/svn/trunk/pokemon.json');
-});
-
-pokemonApp.controller('PokemonCtrl', function ($scope, PokemonFactory){
-    //$scope.pokemon = [{id: 1, name: "Bulbasaur"}];
-
-        PokemonFactory.then(function(data){
-            $scope.pokemon = data.mn;
-        },
-        function(error){
-            alert("Pokemon data retrieval was not super effective. :(");
-         });
-}); */
-
-
-
-
 pokemonApp.controller('PokemonController', function($scope, $http){
     $scope.pokemon = [];
 
     $http.get('app/pkmn.json')
         .success(function(results){
             $scope.pokemon = results;
-            alert("this worked");
-        });
+        })
+		.failure(function(){
+			alert('ruh roh');	
+		});
 
-    //$scope.pokemon = [{num: 1}, {num: 2}, {num: 3}];
-    alert("we got here");
 });
